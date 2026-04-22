40254e982eccf77cda835651a094b9954da8ca07
originateai <james.storey@outlook.com.au>
2026-04-22 17:54:59 +1000

    Add files via upload

--- netlify/functions/get-config.js
+++ netlify/functions/get-config.js
@@ -1,16 +1,11 @@
-exports.handler = async (event) => {
-  // Only serve config to same-origin requests
-  const origin = event.headers.origin || event.headers.referer || '';
-
+exports.handler = async () => {
   return {
     statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-      'Cache-Control': 'no-store',
-    },
+    headers: { 'Content-Type': 'application/json', 'Cache-Control': 'no-store' },
     body: JSON.stringify({
       supabaseUrl:  process.env.SUPABASE_URL,
       supabaseAnon: process.env.SUPABASE_ANON_KEY,
+      googleMapsKey: process.env.GOOGLE_MAPS_API_KEY,
     })
   };
 };
